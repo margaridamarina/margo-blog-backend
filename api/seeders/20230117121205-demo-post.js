92145df1791d85a39f298ba7ceacac1121fc8372
Margarida <margaridammcm@gmail.com>
2023-01-17 09:38:50 -0300

    create seeder

---
+++ api/seeders/20230117121205-demo-post.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert('Posts', [
+			{
+				title: 'Sustentabilidade é vida',
+				image: 'arvore.com.br',
+				description: 'Cuidar do mundo é tudo',
+				text: 'Era uma vez uma árvore',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				title: 'Cozinhar é vida',
+				image: 'comida.com.br',
+				description: 'Comer, comer',
+				text: 'Comer é muito bom',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				title: 'Meditar é vida',
+				image: 'ohm.com.br',
+				description: 'Vamos meditar',
+				text: 'Imagine um rio que corre',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				title: 'Tecnologia verde',
+				image: 'internet.com.br',
+				description: 'Tech é green, tech é pop',
+				text: 'Salve o mundo através de linhas de código',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}
+	], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete('Posts', null, {})
+  }
+}
