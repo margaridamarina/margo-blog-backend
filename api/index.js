b8d6dcf9fede0e4e0363f4c8d12be2b56b382a65
Margarida <margaridammcm@gmail.com>
2023-01-17 09:39:17 -0300

    add route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require ('express')
-// const routes = require ('./routes')
+const routes = require ('./routes')
 
 const app = express()
 const port = 4000
 
-// routes(app)
+routes(app)
 
 app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
 
