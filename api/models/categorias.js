9b1b213745f58bf94947482702f4fc6abfdff4df
Margarida <margaridammcm@gmail.com>
2023-01-20 10:12:18 -0300

    update migrations with associations

--- api/models/categorias.js
+++ api/models/categorias.js
@@ -3,26 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Posts extends Model {
+  class Categorias extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Posts.hasOne(models.Subcategorias, {
-        foreignKey: 'subcategoria_id'
+      Categorias.belongsTo(models.Subcategorias, {
+        foreignKey: 'categoria_id'
       })
     }
   }
-  Posts.init({
-    title: DataTypes.STRING,
-    image: DataTypes.STRING,
-    description: DataTypes.STRING,
-    text: DataTypes.STRING
+  Categorias.init({
+    title: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Posts',
+    modelName: 'Categorias',
   });
-  return Posts;
+  return Categorias;
 };
--- api/models/subcategorias.js
+++ api/models/subcategorias.js
@@ -3,26 +3,26 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Posts extends Model {
+  class Subcategorias extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Posts.hasOne(models.Subcategorias, {
+      Subcategorias.hasOne(models.Categorias, {
+        foreignKey: 'categoria_id'
+      })
+      Subcategorias.belongsTo(models.Posts, {
         foreignKey: 'subcategoria_id'
       })
     }
   }
-  Posts.init({
-    title: DataTypes.STRING,
-    image: DataTypes.STRING,
-    description: DataTypes.STRING,
-    text: DataTypes.STRING
+  Subcategorias.init({
+    title: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Posts',
+    modelName: 'Subcategorias',
   });
-  return Posts;
+  return Subcategorias;
 };
