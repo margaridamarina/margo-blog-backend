7a250bff974b5b6172c58b810ac6300f34170a8c
Margarida <margaridammcm@gmail.com>
2023-01-21 18:03:08 -0300

    fix cors problem

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 const express = require ('express')
 const routes = require ('./routes')
+const cors = require('cors')
 
 const app = express()
+
+app.use(
+    cors({
+        origin:"http://localhost:3007",
+        methods:["GET", "POST", "PUT", "DELETE"]
+    })
+)
+
 const port = 4000
 
 routes(app)
+
 
 app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
 
