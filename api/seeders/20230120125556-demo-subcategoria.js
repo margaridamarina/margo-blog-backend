5b2c1a5c1647309c799bd7c01fd44c9305e3ce2e
Margarida <margaridammcm@gmail.com>
2023-01-21 14:04:12 -0300

    fix seeders

--- api/seeders/20230120125556-demo-subcategoria.js
+++ api/seeders/20230120125556-demo-subcategoria.js
@@ -3,25 +3,25 @@ module.exports = {
 		return queryInterface.bulkInsert('Subcategorias', [
 			{
 				title: 'Reciclagem',
-        categoria_id: 1,
+        		categoria_id: 1,
 				createdAt: new Date(),
 				updatedAt: new Date()
 			},
 			{
 				title: 'Saudável',
-        categoria_id: 2,
+        		categoria_id: 2,
 				createdAt: new Date(),
 				updatedAt: new Date()
 			},
 			{
 				title: 'Meditação',
-        categoria_id: 3,      
+        		categoria_id: 3,				
 				createdAt: new Date(),
 				updatedAt: new Date()
 			},
 			{
 				title: 'Green tech',
-        categoria_id: 4,
+        		categoria_id: 4,
 				createdAt: new Date(),
 				updatedAt: new Date()
 			}
--- api/seeders/20230120125600-demo-post.js
+++ api/seeders/20230120125600-demo-post.js
@@ -6,8 +6,8 @@ module.exports = {
 				image: 'arvore.com.br',
 				description: 'Cuidar do mundo é tudo',
 				text: 'Era uma vez uma árvore',
-        subcategoria_id: 1,
-        createdAt: new Date(),
+				subcategoria_id: 1,
+        		createdAt: new Date(),
 				updatedAt: new Date()
 			},
 			{
@@ -15,7 +15,7 @@ module.exports = {
 				image: 'comida.com.br',
 				description: 'Comer, comer',
 				text: 'Comer é muito bom',
-        subcategoria_id: 2,
+				subcategoria_id: 2,
 				createdAt: new Date(),
 				updatedAt: new Date()
 			},
@@ -24,8 +24,8 @@ module.exports = {
 				image: 'ohm.com.br',
 				description: 'Vamos meditar',
 				text: 'Imagine um rio que corre',
-        subcategoria_id: 3,
-        createdAt: new Date(),
+				subcategoria_id: 3,
+		        createdAt: new Date(),
 				updatedAt: new Date()
 			},
 			{
@@ -33,7 +33,7 @@ module.exports = {
 				image: 'internet.com.br',
 				description: 'Tech é green, tech é pop',
 				text: 'Salve o mundo através de linhas de código',
-        subcategoria_id: 4,
+				subcategoria_id: 4,
 				createdAt: new Date(),
 				updatedAt: new Date()
 			}
