1411d72524465386903d57e1f5b2f5f5bede3c3c
Margarida <margaridammcm@gmail.com>
2023-01-26 20:05:49 -0300

    create new controllers

---
+++ api/controllers/LojaController.js
@@ -0,0 +1,94 @@
+const database = require('../models')
+
+class LojaController {
+  // static async pegaLojasAtivas(req, res){
+  //   try {
+  //     const LojasAtivas = await database.Lojas.findAll()
+  //     return res.status(200).json(LojasAtivas)  
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
+  // }
+
+  static async pegaTodasAsLojas(req, res){
+    try {
+      const TodasAsLojas = await database.Lojas.findAll()
+      return res.status(200).json(TodasAsLojas)  
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async pegaUmaLoja(req, res) {
+    const { LojaId } = req.params
+    try {
+      const UmaLoja = await database.Lojas.findOne( { 
+        where: { 
+          id: Number(LojaId) 
+        }
+      })
+      return res.status(200).json(UmaLoja)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async criaLoja(req, res) {
+    const novaLoja = req.body
+    try {
+      const novaLojaCriado = await database.Lojas.create(novaLoja)
+      return res.status(200).json(novaLojaCriado)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async atualizaLoja(req, res) {
+    const { LojaId } = req.params
+    const novasInfos = req.body
+    try {
+      await database.Lojas.update(novasInfos, { where: { id: Number(LojaId) }})
+      const LojaAtualizado = await database.Lojas.findOne( { where: { id: Number(LojaId) }})
+      return res.status(200).json(LojaAtualizado)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async apagaLoja(req, res) {
+    const { LojaId } = req.params
+    try {
+      await database.Lojas.destroy({ where: { id: Number(LojaId) }})
+      return res.status(200).json({ mensagem: `id ${LojaId} deletado` })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  // static async restauraLoja(req, res) {
+  //   const { LojaId } = req.params
+  //   try {
+  //     await database.Lojas.restore({ where: { id: Number(LojaId) }})
+  //     return res.status(200).json({ mensagem: `id ${LojaId} restaurado` })
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
+  // }
+
+
+  static async pegaTodasAsLojasDeUmaSubcategoria(req, res){
+    const { subcategoriaTitle } = req.params
+    try {
+      const TodasAsLojasDeUmaSubcategoria = await database.Lojas.findAll({
+        where: { 
+          subcategoria_title: String(subcategoriaTitle) 
+        }
+      })
+      return res.status(200).json(TodasAsLojasDeUmaSubcategoria)  
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+}
+
+module.exports = LojaController
---
+++ api/controllers/ReceitasController.js
@@ -0,0 +1,94 @@
+const database = require('../models')
+
+class ReceitaController {
+  // static async pegaReceitasAtivas(req, res){
+  //   try {
+  //     const ReceitasAtivas = await database.Receitas.findAll()
+  //     return res.status(200).json(ReceitasAtivas)  
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
+  // }
+
+  static async pegaTodasAsReceitas(req, res){
+    try {
+      const TodasAsReceitas = await database.Receitas.findAll()
+      return res.status(200).json(TodasAsReceitas)  
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async pegaUmaReceita(req, res) {
+    const { ReceitaId } = req.params
+    try {
+      const UmaReceita = await database.Receitas.findOne( { 
+        where: { 
+          id: Number(ReceitaId) 
+        }
+      })
+      return res.status(200).json(UmaReceita)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async criaReceita(req, res) {
+    const novaReceita = req.body
+    try {
+      const novaReceitaCriado = await database.Receitas.create(novaReceita)
+      return res.status(200).json(novaReceitaCriado)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async atualizaReceita(req, res) {
+    const { ReceitaId } = req.params
+    const novasInfos = req.body
+    try {
+      await database.Receitas.update(novasInfos, { where: { id: Number(ReceitaId) }})
+      const ReceitaAtualizado = await database.Receitas.findOne( { where: { id: Number(ReceitaId) }})
+      return res.status(200).json(ReceitaAtualizado)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async apagaReceita(req, res) {
+    const { ReceitaId } = req.params
+    try {
+      await database.Receitas.destroy({ where: { id: Number(ReceitaId) }})
+      return res.status(200).json({ mensagem: `id ${ReceitaId} deletado` })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  // static async restauraReceita(req, res) {
+  //   const { ReceitaId } = req.params
+  //   try {
+  //     await database.Receitas.restore({ where: { id: Number(ReceitaId) }})
+  //     return res.status(200).json({ mensagem: `id ${ReceitaId} restaurado` })
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
+  // }
+
+
+  static async pegaTodasAsReceitasDeUmaSubcategoria(req, res){
+    const { subcategoriaTitle } = req.params
+    try {
+      const TodasAsReceitasDeUmaSubcategoria = await database.Receitas.findAll({
+        where: { 
+          subcategoria_title: String(subcategoriaTitle) 
+        }
+      })
+      return res.status(200).json(TodasAsReceitasDeUmaSubcategoria)  
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+}
+
+module.exports = ReceitaController
